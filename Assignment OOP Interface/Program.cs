183c68db6c98f97cb03fd277f02485bc9fbd7645
mohammedelgarhy95@gmail.com <mohammedelgarhy95@gmail.com>
2025-01-09 01:26:27 +0200

    Part 01

--- Assignment OOP Interface/Program.cs
+++ Assignment OOP Interface/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Part 01
+
+            // Q 1 : b) To define a blueprint for a class
+            // Q 2 : a) private
+            // Q 3 : b) No
+            // Q 4 : b) Yes, interfaces can inherit from multiple interfaces
+            // Q 5 : d) implements
+            // Q 6 : a) Yes
+            // Q 7 : b) No, all members are implicitly public
+            // Q 8 : b) To provide a clear separation between interface and class members
+            // Q 9 : b) No, interfaces cannot have constructors
+           // Q 10 : c) By separating interface names with commas
+
+
+            #endregion
         }
     }
 }
